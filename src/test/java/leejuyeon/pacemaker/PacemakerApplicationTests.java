c32348a5ab3f8a0e6d696cc7340cc42901ac0a3e
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-08-20 23:41:24 +0900

    chore : project setup

---
+++ src/test/java/leejuyeon/pacemaker/PacemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package leejuyeon.pacemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacemakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
