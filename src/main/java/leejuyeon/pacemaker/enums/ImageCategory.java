9a45ba0e5b88b2fac7d7efde6828604514e17b09
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-08-26 01:44:50 +0900

    chore : add enums
    ExerciseBodyPart
    ExerciseGoal
    ImageCategory

---
+++ src/main/java/leejuyeon/pacemaker/enums/ImageCategory.java
@@ -0,0 +1,9 @@
+package leejuyeon.pacemaker.enums;
+
+public enum ImageCategory {
+  ARRIVAL,      // 도착인증
+  COMPLETION,   // 완료인증
+  INBODY,       // 인바디
+  DIET,         // 식단
+  OTHER         // 기타
+}
