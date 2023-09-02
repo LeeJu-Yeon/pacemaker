200410f5d72447da54a15128dfd9a0f52f543565
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-09-02 19:40:59 +0900

    refactor : Post-Image 엔티티 관계 수정
    운동인증게시물과 이미지 테이블의 관계변화에 따른 코드 수정

--- src/main/java/leejuyeon/pacemaker/entity/Image.java
+++ src/main/java/leejuyeon/pacemaker/entity/Image.java
@@ -2,14 +2,10 @@ package leejuyeon.pacemaker.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
-import leejuyeon.pacemaker.enums.ImageCategory;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -22,13 +18,7 @@ public class Image {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
-  private Post post;
-
   private String url;
-
-  @Enumerated(EnumType.STRING)
-  private ImageCategory category;
 
   @CreatedDate
   private LocalDateTime createdAt;
